6b548f6b933c240ca36174af1f6c02ac61616e5e
System Administrator <root@Caos-MacBook-Pro.local>
2023-03-14 23:31:08 +0900

    update

--- main.js
+++ main.js
@@ -32,8 +32,42 @@ const users = [
     },
 ]
 
-// Lọc filter return trả về đối tượng có giá trị cần tìm kiếm
+// 1. Lọc filter:  return về các object có giá trị cần tìm kiếm có first_name là Jack và Bukayo
 const filterUser = users.filter((user) => {
     return user.first_name === "Jack" || user.first_name === "Bukayo";
 })
-console.log({ filterUser })
+console.log({ filterUser });
+
+
+//2. Lọc Find: giống filter nhưng, return về object đâù tiên thoả mãn
+const findUser = users.find((user) => {
+    return user.first_name === "Jack" || user.first_name === "Bukayo";
+})
+console.log({ findUser });
+
+//3. Lọc Map: 
+// Mục đích: thay đổi array hiện tại
+//Ví dụ 1:
+const customUsers = users.map((user) => {
+    return user.email;
+})
+console.log({ customUsers });
+// giá trị trả về
+//customUsers:Array(5)
+// 0: "caosang92@gmail.com"
+// 1: "caosang94@gmail.com"
+// 2: "caosang93@gmail.com"
+// 3:"caosang99@gmail.com"
+// 4:"caosang2000@gmail.com"
+//ví dụ 2
+//Ví dụ 1:
+const customUsers2 = users.map((user) => {
+    return {
+        ...users,
+        last_name: `${user.last_name} Edited`
+    }
+});
+console.log({ customUsers2 });
+//4. 
+
+
